299e4a0d9a8432ec5a3a82d9f445a6b00601b0ba
dongchul <sah3122@naver.com>
2020-02-10 13:04:41 +0900

    Add JPA Naemd Query

--- src/main/java/me/study/datajpa/repository/MemberRepository.java
+++ src/main/java/me/study/datajpa/repository/MemberRepository.java
@@ -2,11 +2,17 @@ package me.study.datajpa.repository;
 
 import me.study.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsername(String username);
+
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query(name = "Member.findByUsername")
+    List<Member> findNamedQueryByUsername(@Param("username") String username);
 }
