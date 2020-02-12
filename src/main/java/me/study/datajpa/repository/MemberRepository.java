57851ac4069124ddf01ea3dac6ea06303a322da3
dongchul <sah3122@naver.com>
2020-02-12 12:46:50 +0900

    Add Parameter Binding

--- src/main/java/me/study/datajpa/repository/MemberRepository.java
+++ src/main/java/me/study/datajpa/repository/MemberRepository.java
@@ -25,4 +25,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select new me.study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
     List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+    List<Member> findListByUsername(String username);
+
+    Member findMebmerByUsername(String username);
 }
--- src/test/java/me/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/me/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -131,6 +132,20 @@ class MemberRepositoryTest {
         List<MemberDto> memberDto = memberRepository.findMemberDto();
         for (MemberDto dto : memberDto) {
             System.out.println("dto = " + dto);
+        }
+    }
+
+    @Test
+    void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> members = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member member : members) {
+            System.out.println("member = " + member);
         }
     }
 }
