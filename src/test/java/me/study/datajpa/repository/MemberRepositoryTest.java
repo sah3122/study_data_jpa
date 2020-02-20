f06d06271c47bb74a0fdfa96de800bc68819d7b8
dongchul <sah3122@naver.com>
2020-02-20 13:11:29 +0900

    Add EntityGraph (Fetch Join)

--- src/test/java/me/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/me/study/datajpa/repository/MemberRepositoryTest.java
@@ -221,4 +221,27 @@ class MemberRepositoryTest {
 
 
     }
+
+    @Test
+    void findMemberLazy() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //List<Member> members = memberRepository.findAll();
+        List<Member> members = memberRepository.findMemberFetchJoin();
+
+        for (Member member : members) {
+            System.out.println(member.getUsername());
+            System.out.println(member.getTeam().getName());
+        }
+
+    }
 }
