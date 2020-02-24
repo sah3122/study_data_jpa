47010ab0dd25424fb3eade8647595164983b46a9
dongchul <sah3122@naver.com>
2020-02-24 12:53:30 +0900

    Add Domain Class Converter

---
+++ src/main/java/me/study/datajpa/controller/MemberController.java
@@ -0,0 +1,34 @@
+package me.study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.study.datajpa.entity.Member;
+import me.study.datajpa.repository.MemberRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        Member member = new Member("member1", 10);
+        memberRepository.save(member)
+    }
+}
